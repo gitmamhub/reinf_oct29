f474cdcf77ff629a02852ba64d5bfb4d41370a01
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-10-29 15:07:22 -0400

    done the exercise

---
+++ exercise1.js
@@ -0,0 +1,14 @@
+var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
+
+
+console.log("Todo:");
+
+for(var i = 0; i < items.length; i++){
+  console.log(i +': ' + items[i]);
+}
+
+// Todo:
+// 1: purchase domain
+// 2: install SSL certificate
+// 3: deploy site
+// 4: celebrate success
